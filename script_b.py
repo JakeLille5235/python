4f651e3df4618b71b77117f0dbe3d5d83776c29c
JakeLille5235 <strictlybusinessjakelille@gmail.com>
2025-07-23 01:00:14 +0800

    Initial commit

---
+++ script_b.py
@@ -0,0 +1,9 @@
+# assignment 2 for week 1
+# take age from input, print future age in 10 years
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+futureAge = int(age) + 10
+print("Hello " + name + ", in 10 years you will be " + str(futureAge) + " years old.")
+
+# pretty basic, but learned input, string concatenation, type conversion
